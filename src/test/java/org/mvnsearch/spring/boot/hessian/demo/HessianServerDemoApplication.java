4c48f193ae800fef88a39300a5cc046c2fd58be7
linux_china <linux_china@hotmail.com>
2016-12-22 11:14:19 +0800

    hessian server demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/hessian/demo/HessianServerDemoApplication.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.spring.boot.hessian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * hessian server demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class HessianServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HessianServerDemoApplication.class, args);
+    }
+}
